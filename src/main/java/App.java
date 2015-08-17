31dd71650239f081cecee3ef26923288906698fc
Summer Brochtrup & jocelyn alsdorf <student@epicodus.com>
2015-08-17 11:06:41 -0700

    Integration tests and routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,71 @@
-import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.Random;
 
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+      staticFileLocation("/public");
+      String layout = "templates/layout.vtl";
+
+      get("/", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+
+        model.put("template", "templates/welcome.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
 
+      post("/welcome", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
 
+        String inputtedUsername = request.queryParams("username");
+        request.session().attribute("username", inputtedUsername);
+
+        model.put("username", inputtedUsername);
+        model.put("template", "templates/welcome.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+
+      // get("/home", (request, response) -> {
+      //   HashMap<String, Object> model = new HashMap<String, Object>();
+      //
+      //   // String inputtedUsername = request.queryParams("username");
+      //   // request.session().attribute("username", inputtedUsername);
+      //   model.put("username", request.session().attribute("username"));
+      //
+      //   model.put("template", "templates/home.vtl");
+      //   return new ModelAndView(model, layout);
+      // }, new VelocityTemplateEngine());
+
+      // post("/home", (request, response) -> {
+      //   HashMap<String, Object> model = new HashMap<String, Object>();
+      //
+      //   String inputtedUsername = request.queryParams("username");
+      //   request.session().attribute("username", inputtedUsername);
+      //
+      //   model.put("username", inputtedUsername);
+      //   model.put("template", "templates/home.vtl");
+      //   return new ModelAndView(model, layout);
+      // }, new VelocityTemplateEngine());
+
+
+      get("/rectangle", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+
+        //For attribute- 1 arg = getter, 2 args = setter
+        inputtedUsername = request.session().attribute("username");
+        model.put("username", inputtedUsername);
+
+        int length = Integer.parseInt(request.queryParams("length"));
+        int width = Integer.parseInt(request.queryParams("width"));
+
+        Rectangle myRectangle = new Rectangle(length, width);
+        model.put("myRectangle", myRectangle);
+
+        model.put("template", "templates/rectangle.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+    }
 }
